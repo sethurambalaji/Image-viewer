d0f46d941df11836fb38681e4b50197fd896b313
sethurambalaji <73328800+sethurambalaji@users.noreply.github.com>
2021-07-02 13:57:11 +0530

    restrict redirection to profile page from login page by entering url manually

--- src/screens/profile/Profile.js
+++ src/screens/profile/Profile.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import '../profile/Profile.css';
 import Header from '../../common/header/Header';
+import { Redirect } from 'react-router';
 class Profile extends Component {
+    constructor(){
+        super();
+        this.state ={
+            isloggedIn:window.sessionStorage.getItem("access-token")===null?false:true
+        }
+    }
     render() {
         return (
-            <div>
-                <Header />
+            (this.state.isloggedIn === false) ? <Redirect to="/" /> :
+                <div>
+                <Header Header/>
                 <div>Profile Page</div>
-            </div>
+                </div>
+            
+           
         )
     }
 }
